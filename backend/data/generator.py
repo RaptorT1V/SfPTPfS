9212f399d41b921a30c0c4d047f7a01d757d2e03
Chopin <n2108917@edu.misis.ru>
2024-11-09 23:36:19 +0500

    Теперь в генераторе значения могут не только увеличиваться, но и уменьшаться

--- backend/data/generator.py
+++ backend/data/generator.py
@@ -22,14 +22,14 @@ def connect_db():
 def generate_sintering_machine_data(conn):
     cursor = conn.cursor()
 
-    charge_temperature = 40  # температура шихты (С°)
-    speed = 4.6  # скорость (м/мин)
+    charge_temperature = 60  # температура шихты (С°)
+    speed = 5.5  # скорость (м/мин)
     rarefaction = 200  # разрежение (мм. вод. ст.)
 
     while not stop_signal:
-        charge_temperature += random.uniform(1.0, 10.0)
-        speed += random.uniform(0.05, 0.15)
-        rarefaction += random.uniform(10, 150)
+        charge_temperature += random.uniform(-1.0, 4.0)
+        speed += random.uniform(-0.1, 0.4)
+        rarefaction += random.uniform(-10, 30)
 
         cursor.execute("""
             INSERT INTO sinteringMachine (charge_temperature, speed, rarefaction)
@@ -45,13 +45,13 @@ def generate_blast_furnace_data(conn):
     cursor = conn.cursor()
 
     blast_flow_rate = 1000  # объёмный расход дутья (м³/мин)
-    blast_pressure = 1  # давление дутья (кгс/см²)
-    natural_gas_flow_rate = 7000  # объёмный расход природного газа (м³/час)
+    blast_pressure = 3  # давление дутья (кгс/см²)
+    natural_gas_flow_rate = 5000  # объёмный расход природного газа (м³/час)
 
     while not stop_signal:
-        blast_flow_rate += random.uniform(100, 500)
-        blast_pressure += random.uniform(0.1, 0.5)
-        natural_gas_flow_rate += random.uniform(300, 1200)
+        blast_flow_rate += random.uniform(-100, 400)
+        blast_pressure += random.uniform(-0.1, 0.4)
+        natural_gas_flow_rate += random.uniform(-300, 1500)
 
         cursor.execute("""
             INSERT INTO blastFurnace (blast_flow_rate, blast_pressure, natural_gas_flow_rate)
@@ -71,9 +71,9 @@ def generate_flexible_modular_furnace_data(conn):
     power = 20000  # мощность (кВт/ч)
 
     while not stop_signal:
-        argon_flow_rate += random.uniform(50, 250)
-        oxygen_flow_rate += random.uniform(200, 1100)
-        power += random.uniform(1000, 5000)
+        argon_flow_rate += random.uniform(-20, 100)
+        oxygen_flow_rate += random.uniform(-50, 500)
+        power += random.uniform(-1000, 3000)
 
         cursor.execute("""
             INSERT INTO flexibleModularFurnace (argon_flow_rate, oxygen_flow_rate, power)
@@ -88,14 +88,14 @@ def generate_flexible_modular_furnace_data(conn):
 def generate_medium_pressure_boiler_data(conn):
     cursor = conn.cursor()
 
-    temperature = 100  #
-    pressure = 1.0  #
-    steam_output = 50  #
+    temperature = 100  # температура (С°)
+    pressure = 3.5  # давление (МПа)
+    steam_output = 50  # выработка пара (т/ч)
 
     while not stop_signal:
-        temperature += random.uniform(20, 100)
-        pressure += random.uniform(0.1, 1)
-        steam_output += random.uniform(10, 50)
+        temperature += random.uniform(-5, 20)
+        pressure += random.uniform(-0.2, 1)
+        steam_output += random.uniform(-3, 15)
 
         cursor.execute("""
             INSERT INTO mediumPressureBoiler (temperature, pressure, steam_output)
