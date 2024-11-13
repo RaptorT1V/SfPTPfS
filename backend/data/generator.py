10b2cc134c0a52586ae2ee02773bd2e97e821d69
Chopin <n2108917@edu.misis.ru>
2024-11-13 20:43:08 +0500

    Более приближенный к реальности эмулятор завода. Значения теперь изменяются на рандомную величину из промежутка [-x, x + x/8]. В целом, значения растут (на это шанс больше), но очень медленно.

--- backend/data/generator.py
+++ backend/data/generator.py
@@ -22,14 +22,14 @@ def connect_db():
 def generate_sintering_machine_data(conn):
     cursor = conn.cursor()
 
-    charge_temperature = 60  # температура шихты (С°)
-    speed = 5.5  # скорость (м/мин)
-    rarefaction = 200  # разрежение (мм. вод. ст.)
+    charge_temperature = 20  # температура шихты (С°)
+    speed = 2.3  # скорость (м/мин)
+    rarefaction = 190  # разрежение (мм. вод. ст.)
 
     while not stop_signal:
-        charge_temperature += random.uniform(-1.0, 4.0)
-        speed += random.uniform(-0.1, 0.4)
-        rarefaction += random.uniform(-10, 30)
+        charge_temperature += random.uniform(-1.0, 1.125)
+        speed += random.uniform(-0.1, 0.1125)
+        rarefaction += random.uniform(-10, 11.25)
 
         cursor.execute("""
             INSERT INTO sinteringMachine (charge_temperature, speed, rarefaction)
@@ -37,21 +37,21 @@ def generate_sintering_machine_data(conn):
         """, (charge_temperature, speed, rarefaction))
 
         conn.commit()
-        sleep(1.5)
+        sleep(1)
 
 
 # – Доменная печь –
 def generate_blast_furnace_data(conn):
     cursor = conn.cursor()
 
-    blast_flow_rate = 1000  # объёмный расход дутья (м³/мин)
-    blast_pressure = 3  # давление дутья (кгс/см²)
-    natural_gas_flow_rate = 5000  # объёмный расход природного газа (м³/час)
+    blast_flow_rate = 2100  # объёмный расход дутья (м³/мин)
+    blast_pressure = 2.5  # давление дутья (кгс/см²)
+    natural_gas_flow_rate = 5800  # объёмный расход природного газа (м³/час)
 
     while not stop_signal:
-        blast_flow_rate += random.uniform(-100, 400)
-        blast_pressure += random.uniform(-0.1, 0.4)
-        natural_gas_flow_rate += random.uniform(-300, 1500)
+        blast_flow_rate += random.uniform(-100, 112.5)
+        blast_pressure += random.uniform(-0.1, 0.1125)
+        natural_gas_flow_rate += random.uniform(-300, 337.5)
 
         cursor.execute("""
             INSERT INTO blastFurnace (blast_flow_rate, blast_pressure, natural_gas_flow_rate)
@@ -59,21 +59,21 @@ def generate_blast_furnace_data(conn):
         """, (blast_flow_rate, blast_pressure, natural_gas_flow_rate))
 
         conn.commit()
-        sleep(1.5)
+        sleep(1)
 
 
 # – Гибкая модульная печь –
 def generate_flexible_modular_furnace_data(conn):
     cursor = conn.cursor()
 
-    argon_flow_rate = 500  # объёмный расход аргона (л/мин)
-    oxygen_flow_rate = 1000  # объёмный расход кислорода (м³/ч)
-    power = 20000  # мощность (кВт/ч)
+    argon_flow_rate = 555  # объёмный расход аргона (л/мин)
+    oxygen_flow_rate = 999  # объёмный расход кислорода (м³/ч)
+    power = 17123  # мощность (кВт/ч)
 
     while not stop_signal:
-        argon_flow_rate += random.uniform(-20, 100)
-        oxygen_flow_rate += random.uniform(-50, 500)
-        power += random.uniform(-1000, 3000)
+        argon_flow_rate += random.uniform(-20, 22.5)
+        oxygen_flow_rate += random.uniform(-50, 62.5)
+        power += random.uniform(-1000, 1125)
 
         cursor.execute("""
             INSERT INTO flexibleModularFurnace (argon_flow_rate, oxygen_flow_rate, power)
@@ -81,7 +81,7 @@ def generate_flexible_modular_furnace_data(conn):
         """, (argon_flow_rate, oxygen_flow_rate, power))
 
         conn.commit()
-        sleep(1.5)
+        sleep(1)
 
 
 # – Паровой котёл среднего давления –
@@ -89,13 +89,13 @@ def generate_medium_pressure_boiler_data(conn):
     cursor = conn.cursor()
 
     temperature = 100  # температура (С°)
-    pressure = 3.5  # давление (МПа)
-    steam_output = 50  # выработка пара (т/ч)
+    pressure = 3.9  # давление (МПа)
+    steam_output = 54  # выработка пара (т/ч)
 
     while not stop_signal:
-        temperature += random.uniform(-5, 20)
-        pressure += random.uniform(-0.2, 1)
-        steam_output += random.uniform(-3, 15)
+        temperature += random.uniform(-5, 6.25)
+        pressure += random.uniform(-0.2, 0.125)
+        steam_output += random.uniform(-3, 3.375)
 
         cursor.execute("""
             INSERT INTO mediumPressureBoiler (temperature, pressure, steam_output)
@@ -103,7 +103,7 @@ def generate_medium_pressure_boiler_data(conn):
         """, (temperature, pressure, steam_output))
 
         conn.commit()
-        sleep(1.5)
+        sleep(1)
 
 
 def main():
